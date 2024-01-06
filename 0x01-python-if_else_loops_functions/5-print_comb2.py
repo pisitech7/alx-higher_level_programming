9e85f6c1b119e9c66d4c6c4b7562d74a9ec88059
pisitech7 <omolokevin7@gmail.com>
2024-01-06 07:05:14 -0800

    edited code

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
 # Printing numbers from 0 to 99 in the specified format
 for i in range(100):
     if i < 10:
-        print(f"0{i}", end=", " if i < 99 else "\n")
+        print('{:02d}'.format(i), end=", " if i < 99 else "\n")
     else:
-        print(f"{i}", end=", " if i < 99 else "\n")
+        print('{}'.format(i), end=", " if i < 99 else "\n")
