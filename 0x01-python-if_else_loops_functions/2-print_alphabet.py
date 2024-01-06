7caebd729c7ab1d137171a8d01304179f4bf0db5
pisitech7 <omolokevin7@gmail.com>
2024-01-06 03:42:12 -0800

    a program that prints the ASCII alphabet, in lowercase

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for char in range(ord('a'), ord('z') + 1):
-    print(chr(char), end='')
+# Using a loop to print the lowercase ASCII alphabet
+for i in range(ord('a'), ord('z') + 1):
+    print(chr(i), end='')
+
+# Output: abcdefghijklmnopqrstuvwxyz
