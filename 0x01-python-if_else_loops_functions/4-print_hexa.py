56fb58c3e8cabbf4ec67057fd26608ac465d63c6
pisitech7 <omolokevin7@gmail.com>
2024-01-06 06:41:58 -0800

    edited code

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -3,6 +3,6 @@
 # Printing numbers from 0 to 99 in the specified format
 for i in range(100):
     if i < 10:
-        print(f"0{i}", end=", " if i < 99 else "\n")
+        print('{:02d}'.format(i), end=", " if i < 99 else "\n")
     else:
-        print(f"{i}", end=", " if i < 99 else "\n")
+        print('{:d}'.format(i), end=", " if i < 99 else "\n")
