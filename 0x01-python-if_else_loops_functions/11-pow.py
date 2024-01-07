9809460f348721e9359568832c4be10879a4efe2
pisitech7 <omolokevin7@gmail.com>
2024-01-07 02:43:43 -0800

    a function that computes a to the power of b and return the value.

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 def pow(a, b):
-    # Initialize the result to 1
-    result = 1
-
     # Handle the cases when the exponent (b) is positive, negative, or zero
-    if b > 0:
+    if b == 0:
+        return 1
+    elif b > 0:
+        result = 1
         for i in range(b):
             result *= a
-    elif b < 0:
+        return result
+    else:
+        result = 1.0
         for i in range(abs(b)):
             result /= a
-    # If the exponent is zero, the result is 1
-    else:
-        return 1
-
-    return result
+        return result
 
 # Test the function
 result = pow(2, 3)
 print("Result:", result)
+
