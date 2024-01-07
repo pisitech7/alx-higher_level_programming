61db749fae033ebc1390606359dd398bf97a8b63
pisitech7 <omolokevin7@gmail.com>
2024-01-07 02:56:33 -0800

    edited code

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
 def add(a, b):
-    # Calculate the sum of a and b
-    result = a + b
-
-    # Return only the last digit of the result
-    return result % 10
-
-# Test the function
-result = add(2, 3)
-print(result)
-
+    return a + b
