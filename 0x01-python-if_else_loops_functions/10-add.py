c071cacd8c958772228f5bd914d0efad5fe3f18a
pisitech7 <omolokevin7@gmail.com>
2024-01-07 02:23:55 -0800

    a function that adds two integers and returns the resul

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def add(a, b):
-    return a + b
+    # Calculate the sum of a and b
+    result = a + b
+
+    # Return only the last digit of the result
+    return result % 10
 
 # Test the function
 result = add(2, 3)
-print("Result:", result)
-
+print(result)
 
