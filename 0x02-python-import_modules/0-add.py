7128801c066f9c605f3122680f4e815ad7a8ae0d
pisitech7 <omolokevin7@gmail.com>
2024-01-08 14:58:26 -0800

    edited code error

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
+from add_0 import add
 a = 1
 b = 2
-add = __import__('add_0').add
-result = add(a, b)
-print("{:d} + {:d} = {:d}".format(a, b, result))
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
