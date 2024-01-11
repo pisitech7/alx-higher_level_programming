128387d890e579a0a5728660e77f590cf40c7349
pisitech7 <omolokevin7@gmail.com>
2024-01-11 04:20:51 -0800

    edited code

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,14 +1,14 @@
-#!/usr/bin/python3.8.x 
+#!/usr/bin/python3.8.x
 import dis
 import os
 
 def print_hidden_names():
-if __name__ == "__main__":
-    file_path = 'hidden_4.pyc'
-    if os.path.exists(file_path):
-        with open(file_path, 'rb') as file:
+    if os.path.exists('hidden_4.pyc'):
+        with open('hidden_4.pyc', 'rb') as file:
             code = file.read()
             dis.dis(code)
     else:
         print("File 'hidden_4.pyc' not found.")
+
+if __name__ == "__main__":
     print_hidden_names()
