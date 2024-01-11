59c2b741273e28c2531774fa5b9e52e9395b98b2
pisitech7 <omolokevin7@gmail.com>
2024-01-11 04:06:49 -0800

    edited code with if namme == main ()

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,11 +3,10 @@ a = 10
 b = 5
 
 from calculator_1 import add, sub, mul, div
-
-add_result = add(a, b)
-sub_result = sub(a, b)
-mul_result = mul(a, b)
-div_result = div(a, b)
-
-result_format = "{} + {} = {}\n{} - {} = {}\n{} * {} = {}\n{} / {} = {}"
-print(result_format.format(a, b, add_result, a, b, sub_result, a, b, mul_result, a, b, div_result))
+if __name__ == '__main__':
+    add_result = add(a, b)
+    sub_result = sub(a, b)
+    mul_result = mul(a, b)
+    div_result = div(a, b)
+    result_format = "{} + {} = {}\n{} - {} = {}\n{} * {} = {}\n{} / {} = {}"
+    print(result_format.format(a, b, add_result, a, b, sub_result, a, b, mul_result, a, b, div_result))
