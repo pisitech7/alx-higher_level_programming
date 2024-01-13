260c07de2194a9f3e3084cea5874fda200496fde
pisitech7 <omolokevin7@gmail.com>
2024-01-13 02:35:38 -0800

    edited code

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,2 @@
 #!/usr/bin/python3
-str1 = "object-oriented"
-str2 = " programming"
-str3 = " with"
-str4 = " Python"
-print(str1 + str3 + str2 + str4)
+print("".join(["object-oriented", " with", " programming", " Python"]))
