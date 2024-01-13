830a9a16eb3713e154443196cd667e992b0e76dc
pisitech7 <omolokevin7@gmail.com>
2024-01-13 00:15:57 -0800

    edited code

--- 0x04-python-more_data_structures/0-main.py
+++ 0x04-python-more_data_structures/0-main.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-square_matrix_simple = __import__('0-square_matrix_simple').square_matrix_simple
+def square_matrix_simple(matrix=[]):
+    new_matrix = matrix.copy()
 
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+    for i in range(len(matrix)):
+        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
 
-new_matrix = square_matrix_simple(matrix)
-print(new_matrix)
-print(matrix)
+    return (new_matrix)
